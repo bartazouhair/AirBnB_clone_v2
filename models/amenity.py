7be5215f4e510dc87964be07ebfc6f779203639b
sabrallah <sabrallah1991@gmail.com>
2023-09-17 01:17:39 -0700

    fix amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" State Module for the HBNB project """
+""" So, State Module for the HBNB project """
 from os import getenv
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
@@ -7,8 +7,8 @@ from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """class for amenities"""
-    __tablename__ = 'amenities'
+    """It's class for amenities"""
+    _tablename_ = 'amenities'
     if getenv("HBNB_TYPE_STORAGE") == "db":
         name = Column(String(128), nullable=False)
         place_amenities = relationship("Place", secondary="place_amenity",
