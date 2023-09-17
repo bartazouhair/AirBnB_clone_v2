6dba7cdf1ee48a0a4d6eb3e58c1b9f7441e7c47e
bartazouhair <bartazouhair@gmail.com>
2023-09-17 02:49:16 -0700

    File UPDATE INIT FIX

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""c'est un module pour instatioation objet pour cette classe"""
 
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
