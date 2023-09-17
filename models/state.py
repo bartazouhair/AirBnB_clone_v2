58845a77c028f1d8d2ffe9077ead4059618b574a
bartazouhair <bartazouhair@gmail.com>
2023-09-17 02:16:03 -0700

    FILE STATE

--- models/state.py
+++ models/state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" hhjkkjkjkllksyyyskkslslssms """
+""" State Module for HBNB project """
 from os import getenv
 from models.base_model import BaseModel, Base
 from models.city import City
@@ -9,8 +9,8 @@ import models
 
 
 class State(BaseModel, Base):
-    """ khlgslsmsmsmsssmll """
-    _tablename_ = 'states'
+    """ State class """
+    __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     cities = relationship("City",
                           backref="state", cascade="all, delete,delete-orphan")
@@ -18,7 +18,7 @@ class State(BaseModel, Base):
     if getenv('HBNB_TYPE_STORAGE') != "db":
         @property
         def cities(self):
-            """ So returns hghggslmssmmml """
+            """ returns list of City instances related to state """
             from models import storage
             list_cities = []
             for city in storage.all(City).values():
