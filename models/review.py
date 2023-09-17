ddb041f8fa0b4f8728f1eac83c13a84be61d8802
sabrallah <sabrallah1991@gmail.com>
2023-09-17 02:42:44 -0700

    fix review

--- models/review.py
+++ models/review.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
+""" controle le module de hbnb projet """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 
 
 class Review(BaseModel, Base):
-    """ Review class to store review information """
+    """ controle le classe de stockage de conrole d'information """
     __tablename__ = 'reviews'
     text = Column(String(1024), nullable=False)
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
