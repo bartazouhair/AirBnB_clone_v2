1b21e75b7f7d776c8a0bb3db70ad9bd0f65afde6
sabrallah <sabrallah1991@gmail.com>
2023-09-17 02:11:16 -0700

    fix city

--- models/city.py
+++ models/city.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" It's City Module for HBNB project """
+""" City Module for HBNB project """
 from os import getenv
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
@@ -7,8 +7,8 @@ from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """ Oy's The city class, contains state ID & name """
-    _tablename_ = "cities"
+    """ The city class, contains state ID & name """
+    __tablename__ = "cities"
     if getenv("HBNB_TYPE_STORAGE") == "db":
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
