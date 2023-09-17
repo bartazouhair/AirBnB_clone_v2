43789a8228ffcd316859144cc9ede957fc555430
bartazouhair <bartazouhair@gmail.com>
2023-09-17 01:21:34 -0700

    Fixe base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module defines a base class for all models in our hbnb clone"""
+"""It's module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
 import sys
@@ -10,13 +10,13 @@ Base = declarative_base()
 
 
 class BaseModel:
-    """A base class for all hbnb models"""
+    """So A base class for all hbnb models"""
     id = Column(String(60), primary_key=True, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
     updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
+        """It's gInstatntiates a new model"""
         self.id = str(uuid.uuid4())
         if not kwargs:
             from models import storage
@@ -39,19 +39,19 @@ class BaseModel:
                     setattr(self, key, val)
 
     def __str__(self):
-        """Returns a string representation of the instance"""
+        """So, Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
-        """Updates updated_at with current time when instance is changed"""
+        """So, Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
         storage.new(self)
         storage.save()
 
     def to_dict(self):
-        """Convert instance into dict format"""
+        """So, Convert instance into dict format"""
         dictionary = {}
         dictionary.update(self.__dict__)
         dictionary.update({'__class__':
@@ -64,6 +64,6 @@ class BaseModel:
         return dictionary
 
     def delete(self):
-        """delete the current instance from storage"""
+        """ It's delete the current instance from storage"""
         from models import storage
         storage.delete(self)
