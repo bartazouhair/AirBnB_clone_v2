29ff7a46ac9a2adf63ce3baf0cafcbe20e1dfb5c
sabrallah <sabrallah1991@gmail.com>
2023-09-17 02:09:38 -0700

    fix review

--- models/review.py
+++ models/review.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-""" dgfkjdlkgjldjgdlkfgjldkgjf """
-
+""" Review module for the HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 
 
 class Review(BaseModel, Base):
-    """ Igjgjldgjdlkgjdljkfgldkfgldkfgj """
+    """ Review class to store review information """
     __tablename__ = 'reviews'
     text = Column(String(1024), nullable=False)
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
