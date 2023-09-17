353281e9d3746e44d6774c2be5c0d6a20e2a753d
bartazouhair <bartazouhair@gmail.com>
2023-09-17 03:17:07 -0700

    Update fileStorage fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
+"""c'est le module de la definition pour la classe"""
 import json
 import models
 from models.base_model import BaseModel
@@ -12,12 +12,12 @@ from models.user import User
 
 
 class FileStorage:
-    """This class manages storage of hbnb models in JSON format"""
+    """c'est la classe sous fomrat json"""
     __file_path = 'file.json'
     __objects = {}
 
     def all(self, cls=None):
-        """Returns a dictionary of models currently in the storage"""
+        """Retoiur un dictionere de la part storage"""
         if not cls:
             return FileStorage.__objects
         else:
@@ -28,11 +28,11 @@ class FileStorage:
             return newdict
 
     def new(self, obj):
-        """Adds new object to storage dictionary"""
+        """c'est l'ajout d'un fichier"""
         self.__objects["{}.{}".format(type(obj).__name__, obj.id)] = obj
 
     def save(self):
-        """Saves storage dictionary to file"""
+        """c'est la sauvegrade d'un fichier storage"""
         temp = {}
         temp.update(FileStorage.__objects)
         for key, val in temp.items():
@@ -41,7 +41,7 @@ class FileStorage:
             json.dump(temp, f)
 
     def reload(self):
-        """Loads storage dictionary from file"""
+        """c'est le retechargement fichier storag"""
         try:
             with open(FileStorage.__file_path, "r", encoding="UTF8") as fd:
                 for val in json.load(fd).values():
@@ -52,12 +52,12 @@ class FileStorage:
             pass
 
     def delete(self, obj=None):
-        """Delete obj from __objects"""
+        """c'est la suppression d'un objet"""
         if obj:
             key = obj.__class__.__name__ + '.' + obj.id
             if key in self.__objects:
                 del self.__objects[key]
 
     def close(self):
-        """Deserializing the JSON file to objects"""
+        """c'est la fermeture le fichier json"""
         self.reload()
