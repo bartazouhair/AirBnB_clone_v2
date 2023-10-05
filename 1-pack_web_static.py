50aa8cd6bae7d550c6c3c11114f3cb21e424f0c4
bartazouhair <bartazouhair@gmail.com>
2023-10-05 15:54:35 -0700

    Task1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,29 +1,22 @@
-#!/usr/bin/env bash
-# Bash script that sets up your web servers for the deployment of web_static
+#!/usr/bin/python3
+"""
+Fabric script that generates a tgz archive from the contents of the web_static
+folder of the AirBnB Clone repo
+"""
 
-apt-get -y update > /dev/null
-apt-get install -y nginx > /dev/null
+from datetime import datetime
+from fabric.api import local
+from os.path import isdir
 
-# Create all necessary directories and file
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-touch /data/web_static/releases/test/index.html
-echo "Hello World again!" > /data/web_static/releases/test/index.html
 
-# Check if directory current
-if [ -d "/data/web_static/current" ]
-then
-        sudo rm -rf /data/web_static/current
-fi
-# Create a symbolic link test
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# Change ownership to user of envrm
-chown -hR ubuntu:ubuntu /data
-
-# Configure nginx to serve content pointed to by symbolic link to hbnb_static
-sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-
-# Restart serverr
-service nginx restart
-
+def do_pack():
+    """generates a tgz archive"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except:
+        return None
